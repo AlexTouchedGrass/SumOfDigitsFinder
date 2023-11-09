5be045db7fa6eeb426edb3970d292099151db59b
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-11-09 09:45:30 -0500

    Commit

--- src/SumOfDigitsFinder.java
+++ src/SumOfDigitsFinder.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Scanner;
+
+public class SumOfDigitsFinder {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int num = 0;
+        boolean valid = true;
+
+        System.out.println("Please enter a number.");
+        num = scan.nextInt();
+
+        while (valid) {
+        }
     }
 }
