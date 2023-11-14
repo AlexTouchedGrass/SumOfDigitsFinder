9cfb0c636183fa3d42b3d8f0de531f6b46c7b57d
AlexTouchedGrass <144457672+AlexTouchedGrass@users.noreply.github.com>
2023-11-13 20:53:09 -0500

    Update SumOfDigitsFinder.java

--- src/SumOfDigitsFinder.java
+++ src/SumOfDigitsFinder.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class SumOfDigitsFinder {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int num = 0;
+        int sum = 0;
         boolean valid = true;
 
         System.out.println("Please enter a number.");
         num = scan.nextInt();
 
-        while (valid) {
+        if (num < 0) {
+            num = num * -1;
         }
+
+        while (num>0) {
+            sum = sum + num%10;
+            num = num/10;
+        }
+        System.out.println(sum);
     }
 }
